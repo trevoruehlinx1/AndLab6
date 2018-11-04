647a772b70e6e8444b77567ff41074f3282a61ff
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2018-11-04 14:45:02 -0800

    Solution Files

---
+++ TidePredictor/TidePredictor/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+
+namespace TidePredictor
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            string[] testData = Resources.GetStringArray(Resource.Array.test_data_array2);
+            ListAdapter = new ArrayAdapter<string> (this, Resource.Layout.list_item, testData);
+
+            ListView.TextFilterEnabled = true;
+
+            ListView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
+            {
+                Toast.MakeText(Application, ((TextView)args.View).Text, ToastLength.Short).Show();
+            };
+
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
+
